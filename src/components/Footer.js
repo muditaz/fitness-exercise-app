ea25bcdf8e3eae9d8b8f4602be2e004cfa4f9643
Mudit Agrawal <muditagrawalaz@gmail.com>
2023-10-11 23:11:13 +0530

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
-const Footer = () => {
-    return(<div></div>);
-};
+import { Box, Stack } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+    <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} /><h1>FitFab</h1> <h3>All Rights Reserved</h3>
+    </Stack>
+  </Box>
+);
 
 export default Footer;
